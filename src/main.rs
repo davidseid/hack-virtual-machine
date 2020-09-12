4d41e3eaf8536fed729da1e5db98e8f18698fb95
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-12 12:45:27 -0700

    Ignore target files

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,10 @@ fn main() {
     let source = &args[1];
 
     println!("Source intermediary VM code: {}", source);
+
+    // initialize array of filenames
+    // if source is .vm, add it to the filenames
+    // if not, get all the files in it and add it to the array
+
+    let mut source_files = Vec::<String>::new();
 }
