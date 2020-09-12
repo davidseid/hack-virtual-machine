d441041d99527780348938ff81a1eddb888a0d00
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-12 12:38:58 -0700

    Ignore target

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    println!("VM Translator starting up...");
+
+    let args: Vec<String> = env::args().collect();
+    let source = &args[1];
+
+    println!("Source intermediary VM code: {}", source);
 }
