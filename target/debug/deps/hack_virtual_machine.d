a0777549449622fc08f7622705c87f05b7ac5339
David Seidenberg <david.seidenberg@pubnub.com>
2020-09-12 12:32:00 -0700

    Initialize hack-virtual-machine rust project

---
+++ target/debug/deps/hack_virtual_machine.d
@@ -0,0 +1,5 @@
+/Users/david.seidenberg/rust/hack-virtual-machine/target/debug/deps/hack_virtual_machine: src/main.rs
+
+/Users/david.seidenberg/rust/hack-virtual-machine/target/debug/deps/hack_virtual_machine.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hack-virtual-machine.d
@@ -0,0 +1,1 @@
+/Users/david.seidenberg/rust/hack-virtual-machine/target/debug/hack-virtual-machine: /Users/david.seidenberg/rust/hack-virtual-machine/src/main.rs
