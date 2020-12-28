71efd8976c148aba7cc0f6432ec4bb9e7ba76c7a
David Seidenberg <david.seidenberg@pubnub.com>
2020-12-28 12:04:48 -0700

    Stub out Codewriter public methods

---
+++ src/codewriter.rs
@@ -0,0 +1,13 @@
+// CodeWriter translates VM commands into HACK Assembly Code
+
+pub struct CodeWriter {}
+
+pub fn new() {}
+
+pub fn set_filename(filename: &str) {}
+
+pub fn write_arithmetic(command: &str) {}
+
+pub fn write_push_pop(command: Command, segment: &str, index: usize) {}
+
+pub fn close() {}
